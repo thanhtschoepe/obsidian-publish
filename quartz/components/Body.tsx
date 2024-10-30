2d5fc05bb5f1fe3bcafb61bf8fe529d118b6e133
Thanh Tschoepe <ttschoepe@hubspot.com>
2024-10-30 10:52:32 -0500

    Quartz sync: Oct 30, 2024, 10:52 AM

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -4,28 +4,7 @@ import clipboardStyle from "./styles/clipboard.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return (
-    <>
-      <div id="quartz-body">{children}</div>
-      <script
-        src="https://giscus.app/client.js"
-        data-repo="thanhtschoepe/obsidian-publish"
-        data-repo-id="R_kgDOND35Vg"
-        data-category="Announcements"
-        data-category-id="DIC_kwDOND35Vs4Cj1S_"
-        data-mapping="pathname"
-        data-strict="0"
-        data-reactions-enabled="1"
-        data-emit-metadata="0"
-        data-input-position="top"
-        data-theme="dark_tritanopia"
-        data-lang="en"
-        data-loading="lazy"
-        crossorigin="anonymous"
-        async
-      ></script>
-    </>
-  )
+  return <div id="quartz-body">{children}</div>
 }
 
 Body.afterDOMLoaded = clipboardScript
