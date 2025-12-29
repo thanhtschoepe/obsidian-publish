52f7b3e673e889e3837a00db4a9ec2d976d0ac9d
Thanh <thanh.tschoepe@gmail.com>
2025-12-29 13:23:40 -0600

    Quartz sync: Dec 29, 2025, 1:23 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,9 +7,20 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
-    </h2>
+    <>
+      <div class="lava-lamp">
+        <div class="lava-blob"></div>
+        <div class="lava-blob"></div>
+        <div class="lava-blob"></div>
+      </div>
+      <div class="sky-glow">
+        <div class="sky-blob"></div>
+        <div class="sky-blob"></div>
+      </div>
+      <h2 class={classNames(displayClass, "page-title")}>
+        <a href={baseDir}>{title}</a>
+      </h2>
+    </>
   )
 }
 
